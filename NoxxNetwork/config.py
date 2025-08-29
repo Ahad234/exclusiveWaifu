b10edab2e8dc229f1a44ebca5d4c67fe35ae2731
Ahad234 <96462140+Ahad234@users.noreply.github.com>
2025-08-29 16:23:39 +0530

    Update config.py

--- NoxxNetwork/config.py
+++ NoxxNetwork/config.py
@@ -7,18 +7,18 @@ load_dotenv()
 class Config(object):
     LOGGER = True
 
-    OWNER_ID = os.getenv("OWNER_ID")
-    sudo_users = os.getenv("SUDO_USERS").split(",")  # Convert CSV to list
-    GROUP_ID = int(os.getenv("GROUP_ID"))
-    TOKEN = os.getenv("TOKEN")
-    mongo_url = os.getenv("MONGO_URL")
+    OWNER_ID = os.getenv("OWNER_ID","8195241636")
+    sudo_users = os.getenv("SUDO_USERS","5303251380").split(",")  # Convert CSV to list
+    GROUP_ID = int(os.getenv("GROUP_ID",'-1002902949191"))
+    TOKEN = os.getenv("TOKEN","7961956208:AAFp1pTmpTj--sH5CMxEVYFWEh_hYSan0iI")
+    mongo_url = os.getenv("MONGO_URL","mongodb+srv://ahaan:ahaad@ahaan.hgkeruq.mongodb.net/?retryWrites=true&w=majority&appName=ahaan")
     PHOTO_URL = os.getenv("PHOTO_URL").split(",")  # Convert CSV to list
-    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
-    UPDATE_CHAT = os.getenv("UPDATE_CHAT")
-    BOT_USERNAME = os.getenv("BOT_USERNAME")
-    CHARA_CHANNEL_ID = int(os.getenv("CHARA_CHANNEL_ID"))
-    api_id = int(os.getenv("API_ID"))
-    api_hash = os.getenv("API_HASH")
+    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT","https://t.me/WaifuxIvan")
+    UPDATE_CHAT = os.getenv("UPDATE_CHAT","https://t.me/WaifuxDb")
+    BOT_USERNAME = os.getenv("BOT_USERNAME","WaifuxGraber_bot")
+    CHARA_CHANNEL_ID = int(os.getenv("CHARA_CHANNEL_ID","WaifuxDb"))
+    api_id = int(os.getenv("API_ID","22657083"))
+    api_hash = os.getenv("API_HASH","d6186691704bd901bdab275ceaab88f3")
 
 
 class Production(Config):
